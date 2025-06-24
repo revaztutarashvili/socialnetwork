bea91c26fb458f027d12ec5a1ca36b5ac4b5c3fc
“Revaz <generationofjeans@gmail.com>
2025-06-24 23:25:04 +0400

    add spring initializr base

---
+++ src/test/java/com/example/com/socialnetwork/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.com.socialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
